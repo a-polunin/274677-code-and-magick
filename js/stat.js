cfcc4e63cd5f03c6e74783e2d08202201f4ac8c5
bazikk <33804374+bazikk@users.noreply.github.com>
2018-11-19 02:07:45 +0300

    eslint fix

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,4 @@
+'use strict';
 var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 
@@ -5,48 +6,43 @@ var GRAPH_HEIGHT = 150;
 var COLUMN_WIDTH = 40;
 var COLUMN_SPACE = 50;
 var COLUMN_X = 140;
-var COLUMN_Y = 110;
 
 var GAP = 20;
 var NAME_Y = 260;
 
-var renderCloud = function(ctx, x, y, color){
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
-  ctx.fillRect(x,y,CLOUD_WIDTH,CLOUD_HEIGHT);
-}
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
-function getRandomInRange(min, max) {
-  return Math.random() * (max - min + 1) + min;
-}
-
-var renderColumn = function(ctx, time, name, columnCount, maxResult){
-  if(name === 'Вы'){
+var renderColumn = function (ctx, time, name, columnCount, maxResult) {
+  if (name === 'Вы') {
     ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-  }else{
+  } else {
     ctx.fillStyle = 'rgba(0,0,255,' + Math.random() + ')';
   }
 
-  ctx.fillRect(COLUMN_X + ((COLUMN_WIDTH + COLUMN_SPACE)*columnCount), CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP, COLUMN_WIDTH, time * GRAPH_HEIGHT / maxResult);
-}
+  ctx.fillRect(COLUMN_X + ((COLUMN_WIDTH + COLUMN_SPACE) * columnCount), CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP, COLUMN_WIDTH, time * GRAPH_HEIGHT / maxResult);
+};
 
-var renderColumnTime = function(ctx, time, columnCount, maxResult){
+var renderColumnTime = function (ctx, time, columnCount, maxResult) {
   ctx.fillStyle = '#000000';
-  ctx.fillText(Math.floor(time),COLUMN_X + ((COLUMN_WIDTH + COLUMN_SPACE)*columnCount), CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP * 2);
-}
+  ctx.fillText(Math.floor(time), COLUMN_X + ((COLUMN_WIDTH + COLUMN_SPACE) * columnCount), CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP * 2);
+};
 
-var renderColumnName = function(ctx, name, columnCount){
+var renderColumnName = function (ctx, name, columnCount) {
   ctx.fillStyle = '#000000';
-  ctx.fillText(name,COLUMN_X + ((COLUMN_WIDTH + COLUMN_SPACE)*columnCount),NAME_Y);
-}
+  ctx.fillText(name, COLUMN_X + ((COLUMN_WIDTH + COLUMN_SPACE) * columnCount), NAME_Y);
+};
 
-var renderHeading = function(ctx,text,x,y){
+var renderHeading = function (ctx, text, x, y) {
   ctx.font = '16px PT Mono';
   ctx.fillStyle = '#000000';
   ctx.textBaseline = 'hanging';
-  ctx.fillText(text ,x, y);
-}
+  ctx.fillText(text, x, y);
+};
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
   for (var i = 0; i < arr.length; i++) {
@@ -58,18 +54,18 @@ var getMaxElement = function(arr) {
   return maxElement;
 };
 
-window.renderStatistics = function(ctx,names,times){
+window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, 110, 20, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, 100, 10, '#ffffff');
 
-  renderHeading(ctx,'Ура вы победили!' ,120, 30);
-  renderHeading(ctx,'Список результатов:',120, 50);
+  renderHeading(ctx, 'Ура вы победили!', 120, 30);
+  renderHeading(ctx, 'Список результатов:', 120, 50);
 
   var maxTime = getMaxElement(times);
 
-  for(var i = 0;i < times.length; i++){
-    renderColumn(ctx,times[i],names[i],i,maxTime);
-    renderColumnName(ctx,names[i],i);
-    renderColumnTime(ctx,times[i],i,maxTime);
+  for (var i = 0; i < times.length; i++) {
+    renderColumn(ctx, times[i], names[i], i, maxTime);
+    renderColumnName(ctx, names[i], i);
+    renderColumnTime(ctx, times[i], i, maxTime);
   }
-}
+};
