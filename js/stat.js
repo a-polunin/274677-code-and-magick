f2e9d7a7649d2c639cc83715a21837be86a4f1ee
bazikk <33804374+bazikk@users.noreply.github.com>
2018-11-20 21:45:52 +0300

    исправил названия переменных,вынес черный цвет в отдельную константу

--- js/stat.js
+++ js/stat.js
@@ -11,6 +11,7 @@ var COLUMN_X_START = 140;
 var GAP = 20;
 var NAME_Y = 260;
 var HEADING_FONT = '16px PT Mono';
+var BLACK_COLOR = '#000000';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -18,32 +19,32 @@ var renderCloud = function (ctx, x, y, color) {
 };
 
 var renderColumn = function (ctx, time, name, columnCount, maxResult) {
-  var COLUMN_HEIGHT = time * GRAPH_HEIGHT / maxResult;
-  var COLUMN_X = COLUMN_X_START + (COLUMN_WIDTH_AND_SPACE * columnCount);
-  var COLUMN_Y = CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP;
+  var columnHeight = time * GRAPH_HEIGHT / maxResult;
+  var columnX = COLUMN_X_START + (COLUMN_WIDTH_AND_SPACE * columnCount);
+  var columnY = CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP;
 
   ctx.fillStyle = (name === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'rgba(0,0,255,' + Math.random() + ')';
-  ctx.fillRect(COLUMN_X, COLUMN_Y, COLUMN_WIDTH, COLUMN_HEIGHT);
+  ctx.fillRect(columnX, columnY, COLUMN_WIDTH, columnHeight);
 };
 
 var renderColumnTime = function (ctx, time, columnCount, maxResult) {
-  var TIME_X = COLUMN_X_START + (COLUMN_WIDTH_AND_SPACE * columnCount);
-  var TIME_Y = CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP * 2;
+  var timeX = COLUMN_X_START + (COLUMN_WIDTH_AND_SPACE * columnCount);
+  var timeY = CLOUD_HEIGHT - (time * GRAPH_HEIGHT / maxResult) - GAP * 2;
 
-  ctx.fillStyle = '#000000';
-  ctx.fillText(Math.floor(time), TIME_X, TIME_Y);
+  ctx.fillStyle = BLACK_COLOR;
+  ctx.fillText(Math.floor(time), timeX, timeY);
 };
 
 var renderColumnName = function (ctx, name, columnCount) {
-  var NAME_X = COLUMN_X_START + (COLUMN_WIDTH_AND_SPACE * columnCount);
+  var nameX = COLUMN_X_START + (COLUMN_WIDTH_AND_SPACE * columnCount);
 
-  ctx.fillStyle = '#000000';
-  ctx.fillText(name, NAME_X, NAME_Y);
+  ctx.fillStyle = BLACK_COLOR;
+  ctx.fillText(name, nameX, NAME_Y);
 };
 
 var renderHeading = function (ctx, text, x, y) {
   ctx.font = HEADING_FONT;
-  ctx.fillStyle = '#000000';
+  ctx.fillStyle = BLACK_COLOR;
   ctx.textBaseline = 'hanging';
 
   ctx.fillText(text, x, y);
