4b9ab7bc5c54ac7bdfbca6047d178701817ba0a1
bazikk <33804374+bazikk@users.noreply.github.com>
2018-12-15 22:44:55 +0300

    Исправление в методе save

--- js/backend.js
+++ js/backend.js
@@ -1,7 +1,6 @@
 'use strict';
 (function () {
   var createHttpRequest = function (URL, method, onLoadCallback, onErrorCallback, data) {
-    data = data || null;
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
 
@@ -23,7 +22,11 @@
 
     xhr.timeout = 10000;
     xhr.open(method, URL);
-    xhr.send(data);
+    if (method === 'POST') {
+      xhr.send(data);
+    } else {
+      xhr.send();
+    }
   };
 
   window.backend = {
@@ -33,27 +36,7 @@
     },
     save: function (data, onLoad, onError) {
       var URL = 'https://js.dump.academy/code-and-magick';
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-
-      xhr.timeout = 10000;
-      xhr.open('POST', URL);
-      xhr.send(data);
+      createHttpRequest(URL, 'POST', onLoad, onError, data);
     }
   };
 })();
