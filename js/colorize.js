fca827d3f02f2acf40249d3c6b10e97fd8c8d1f0
bazikk <33804374+bazikk@users.noreply.github.com>
2018-12-23 22:29:38 +0300

    Добавил отрисовку похожих магов. Отрисовка происходит не чаще чем один раз в полсекунды

--- js/colorize.js
+++ js/colorize.js
@@ -3,26 +3,31 @@
   var setupWizardCoat = document.querySelector('.setup-wizard .wizard-coat');
   var setupWizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
   var setupFireball = document.querySelector('.setup-fireball-wrap');
+  var coatInput = document.querySelector('input[name="coat-color"]');
+  var eyesInput = document.querySelector('input[name="eyes-color"]');
+  var fireballInput = document.querySelector('input[name="fireball-color"]');
 
   window.colorize = {
     setupWizardCoat: setupWizardCoat,
     setupWizardEyes: setupWizardEyes,
     setupFireball: setupFireball,
+    coatInput: coatInput,
+    eyesInput: eyesInput,
+    fireballInput: fireballInput,
 
     colorizeCoat: function () {
-      var coatInput = document.querySelector('input[name="coat-color"]');
       var coatColor = window.util.getRandomElementFromArray(window.constants.PULL_OF_COAT_COLORS);
       setupWizardCoat.style.fill = coatColor;
       coatInput.value = coatColor;
+      window.updateWizards();
     },
     colorizeEyes: function () {
-      var eyesInput = document.querySelector('input[name="eyes-color"]');
       var eyesColor = window.util.getRandomElementFromArray(window.constants.PULL_OF_EYES_COLORS);
       setupWizardEyes.style.fill = eyesColor;
       eyesInput.value = eyesColor;
+      window.debounce(window.updateWizards)();
     },
     colorizeFireball: function () {
-      var fireballInput = document.querySelector('input[name="fireball-color"]');
       var fireballColor = window.util.getRandomElementFromArray(window.constants.PULL_OF_FIREBALL_COLORS);
       setupFireball.style.backgroundColor = fireballColor;
       fireballInput.value = fireballColor;
--- js/create-wizards.js
+++ js/create-wizards.js
@@ -3,21 +3,54 @@
   document.querySelector('.setup-similar').classList.remove('hidden');
 
   var similarListElement = document.querySelector('.setup-similar-list');
+  var wizards = [];
 
-  var successHandler = function (data) {
+  var getRank = function (wizard) {
+    var rank = 0;
+
+    if (wizard.colorCoat === window.colorize.coatInput.value) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === window.colorize.eyesInput.value) {
+      rank += 1;
+    }
+
+    return rank;
+  };
+
+  var renderWizards = function () {
     var fragment = document.createDocumentFragment();
     var similarWizardTemplate = document.querySelector('#similar-wizard-template')
       .content
       .querySelector('.setup-similar-item');
-
     for (var i = 0; i < window.constants.SIMILAR_WIZARDS_COUNT; i++) {
       var wizardElement = window.util.cloneElement(similarWizardTemplate);
-      var randomInteger = window.util.getRandomInteger(0, data.length - 1);
-
-      fragment.appendChild(fillWizardWithData(wizardElement, data[randomInteger]));
+      var sortedWizards = sortWizards();
+      fragment.appendChild(fillWizardWithData(wizardElement, sortedWizards[i]));
     }
-
+    similarListElement.innerHTML = '';
     window.util.render(similarListElement, fragment);
+  };
+
+  var sortWizards = function () {
+    var sortedWizards = wizards.slice().sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = wizards.indexOf(left) - wizards.indexOf(right);
+      }
+      return rankDiff;
+    });
+
+    return sortedWizards;
+  };
+
+  var updateWizards = window.debounce(function () {
+    renderWizards(sortWizards());
+  });
+
+  var successHandler = function (data) {
+    wizards = data;
+    renderWizards(data);
   };
 
   var fillWizardWithData = function (wizard, data) {
@@ -32,5 +65,6 @@
     return wizard;
   };
 
+  window.updateWizards = updateWizards;
   window.backend.load(successHandler, window.util.createErrorAlert);
 })();
